b85e1129bbdd610db74ff496ef5db9da9c7fa943
srikanth2209 <srikanthkareti7@gmail.com>
2021-10-25 10:58:15 +0530

    comparingFiles

---
+++ src/ComparingStrings/AppRun.java
@@ -0,0 +1,15 @@
+
+package ComparingStrings;
+
+        import org.json.JSONException;
+
+        import java.io.IOException;
+
+public class AppRun
+{
+    public static void main(String[] args) throws JSONException, IOException {
+        InterfaceClass IC = new Logic();
+        IC.FileReaderMethod();
+        IC.JsonFileMethod();
+    }
+}
---
+++ src/ComparingStrings/InterfaceClass.java
@@ -0,0 +1,13 @@
+
+package ComparingStrings;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+public interface InterfaceClass
+{
+    void FileReaderMethod() throws IOException, JSONException;
+
+    void JsonFileMethod() throws IOException, JSONException;
+}
