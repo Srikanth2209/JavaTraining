c99dcd9f5196907150e400b240b0c08c63533f19
srikanth2209 <srikanthkareti7@gmail.com>
2021-11-15 17:46:36 +0530

    Calc

---
+++ src/ReadingData/JsonToExcel.java
@@ -0,0 +1,41 @@
+package ReadingData;
+
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.WritableWorkbookImpl;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JsonToExcel
+{
+    public static void main(String[] args) throws IOException, ParseException, WriteException {
+        FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\HP\\Desktop\\FileReaders\\jsotoxl.xls");
+        WritableWorkbook writableWorkbook= Workbook.createWorkbook(fileOutputStream);
+        WritableSheet writableSheet = writableWorkbook.createSheet("content",0);
+        JSONParser jsonParser = new JSONParser();
+        Object obj = jsonParser.parse(new FileReader("C:\\Users\\HP\\Desktop\\FileReaders\\studentData2.json"));
+        JSONObject jsonObject = (JSONObject) obj;
+        String Mar = (String) jsonObject.get("Name");
+        String Ag = (String) jsonObject.get("Age");
+        String Tm = (String) jsonObject.get("Total_Marks");
+
+        Label l1 = new Label(0,1,Mar);
+        Label l2 = new Label(1,1,Ag);
+        Label l3 = new Label(2,1,Tm);
+        writableSheet.addCell(l1);
+        writableSheet.addCell(l2);
+        writableSheet.addCell(l3);
+        writableWorkbook.write();
+        writableWorkbook.close();
+
+    }
+}
--- src/ReadingData/ReadingDataFromXLS.java
+++ src/ReadingData/ReadingDataFromXLS.java
@@ -2,28 +2,28 @@ package ReadingData;
 
 
 import jxl.Sheet;
-import jxl.Workbook;
-import jxl.read.biff.BiffException;
-import org.json.JSONObject;
-import java.io.*;
+        import jxl.Workbook;
+        import jxl.read.biff.BiffException;
+        import org.json.JSONObject;
+        import java.io.*;
 
 
 public class ReadingDataFromXLS
 {
     public static void main(String[] args) throws IOException, BiffException {
-        File f = new File("C:\\Users\\HP\\Desktop\\FileReaders\\student.xls");
+        File f = new File("C:\\Users\\HP\\Desktop\\FileReaders\\student1.xls");
         FileInputStream fis = new FileInputStream(f);
         Workbook w = Workbook.getWorkbook(fis);
         Sheet s = w.getSheet("Sheet1");
 
-        FileWriter MyWriter = new FileWriter("C:\\Users\\HP\\Desktop\\FileReaders\\studentData.json");
+        FileWriter MyWriter = new FileWriter("C:\\Users\\HP\\Desktop\\FileReaders\\studentData2.json");
         for (int j = 1; j < s.getRows(); j++) {
             JSONObject obj = new JSONObject();
-            // MyWriter.write(String.valueOf(obj.put(s.getCell(0, 0).getContents() +"/"+ s.getCell(1, 0).getContents()+"/" + s.getCell(2, 0).getContents(), s.getCell(0, j).getContents()+"/"+s.getCell(1,j).getContents()+"/"+s.getCell(2,j).getContents())));
             obj.put(s.getCell(0, 0).getContents(), s.getCell(0, j).getContents());
             obj.put(s.getCell(1, 0).getContents(), s.getCell(1, j).getContents());
             obj.put(s.getCell(2, 0).getContents(), s.getCell(2, j).getContents());
             MyWriter.write(obj.toString());
+            MyWriter.write("\n");
             MyWriter.flush();
         }
     }
